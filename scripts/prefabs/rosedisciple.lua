b0470306866bbc9be96cb6128a33673befe91e36
DrSmugleaf <javier.frayluis@gmail.com>
2015-09-21 22:58:03 +0200

    Eating function improvements
    Now it actually makes sense

--- scripts/prefabs/rosedisciple.lua
+++ scripts/prefabs/rosedisciple.lua
@@ -58,34 +58,8 @@ local function onmoisturedelta(inst)
         inst.components.sanity.dapperness = 0 -- Reset the sanity drain to 0
     end
 end
- 
--- Eating Meat Bonus
-local function oneat(inst, food)
-    if food and food.components.edible and food.components.edible.foodtype == FOODTYPE.MEAT then -- If it is food, edible, and meat
-     
-        local healthvalue = food.components.edible.healthvalue -- Shortcuts for having to write only "healthvalue" instead of "food.components.edible.healthvalue"
-        local hungervalue = food.components.edible.foodvalue
-        local sanityvalue = food.components.edible.sanityvalue
-         
-        if healthvalue and not healthvalue == 0 then -- If the food item has a health value, and it isn't 0
-            if healthvalue >= 0 then -- If the food item's health value is over 0
-				inst.components.health:DoDelta(healthvalue * 1.5) -- Multiplies food's health value by 1.5
-            end
-        end
-        if hungervalue and not hungervalue == 0 then
-            if hungervalue >= 0 then
-				inst.components.hunger:DoDelta(hungervalue * 1.5) -- Multiplies food's hunger value by 1.5
-			end
-        end
-        if sanityvalue and not sanityvalue == 0 then
-            if sanityvalue >= 0 then
-				inst.components.sanity:DoDelta(sanityvalue * 1.5) -- Multiplies food's sanity value by 1.5
-			end
-        end
-    end
-end
- 
- 
+
+
 -- This initializes for both clients and the host
 local common_postinit = function(inst)
     inst.MiniMapEntity:SetIcon( "rosedisciple.tex" ) -- Minimap icon
@@ -127,16 +101,19 @@ local master_postinit = function(inst)
      
     -- Hounds don't attack you
     inst:AddTag("hound") -- Add the tag "hound" to the player
-     
-    -- Add the eater component
-    if inst.components.eater == nil then -- If the character doesn't have the component
-		inst:AddComponent("eater") -- Add the component "eater" to let us execute a function when it eats
-    end
 	
 	inst.components.eater.strongstomach = true
 	
-    -- Execute a function when the character eats
-    inst.components.eater:SetOnEatFn(oneat) -- Execute a function when the character eats
+	inst.components.eater.oldEat = inst.components.eater.Eat
+	function inst.components.eater:Eat(food)
+		if self:CanEat(food) then
+			if food.components.edible.foodtype == FOODTYPE.MEAT then
+				food.components.edible.healthvalue = food.components.edible.healthvalue * 1.5
+				food.components.edible.sanityvalue = food.components.edible.sanityvalue * 1.5
+			end
+		end
+		return inst.components.eater:oldEat(food)
+	end
      
 end
 
